4b53a91bb121dbfafdbce750ae99c92a1386b06a
wangpeng <395916019@qq.com>
2017-11-03 09:41:09 +0800

    1.3版本 更新申请试用，并更新编译器为3.0

---
+++ TuZhi/TuZhi/src/main/java/com/tuzhi/application/moudle/applyfortrial/ApplyForTrialContract.java
@@ -0,0 +1,19 @@
+package com.tuzhi.application.moudle.applyfortrial;
+
+import com.tuzhi.application.moudle.basemvp.BasePresenter;
+import com.tuzhi.application.moudle.basemvp.BaseView;
+
+/**
+ * MVPPlugin
+ *  邮箱 784787081@qq.com
+ */
+
+public class ApplyForTrialContract {
+    interface View extends BaseView {
+        void commitSuccess();
+    }
+
+    interface  Presenter extends BasePresenter<View> {
+        void commit(ApplyForTrialBean bean);
+    }
+}
